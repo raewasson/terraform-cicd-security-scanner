30661fd2021549c70228aa6bb0497efe792ec7cd
raewasson <rachaelwasson2000@gmail.com>
2026-05-12 14:02:15 -0500

    feat: generate Markdown security report CI artifact

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -58,3 +58,22 @@ resource "aws_security_group" "wide_open" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+}
