490018917319b9edcfeb7066d25f6e92c85365a9
raewasson <rachaelwasson2000@gmail.com>
2026-05-11 20:57:16 -0500

    feat: deduplication script

---
+++ scanner/scan.py
@@ -0,0 +1,60 @@
+'''
+Trivy and Checkov create a lot of duplicates, this script is a quick poc for deduplication biased towards Trivy findings.
+'''
+
+import json
+
+DEDUP_MAP = {
+    # (resource_type_prefix, keyword) -> deduped issue name
+    ("aws_security_group", "ssh"):        "unrestricted_ssh_ingress",
+    ("aws_security_group", "0.0.0.0:0 to port 22"): "unrestricted_ssh_ingress",
+    ("aws_security_group", "description"): "missing_sg_description",
+}
+
+OWASP_MAP = {
+    "iam": "OWASP AO1:2025 - Broken Access Control",
+    "encryption": "OWASP A04:2025 - Cryptographic Failures",
+    "logging": "OWASP A09:2025 - Security Logging and Alerting Failures"
+}
+
+def parse_results(trivy_path: str, checkov_path: str):
+    with open(trivy_path) as trivy_file:
+      trivy_results = json.load(trivy_file)
+    with open(checkov_path) as checkov_file:
+      checkov_results = json.load(checkov_file)
+    with open("deduped_results.json", "w") as f: f.write(json.dumps(deduplicate_results(trivy_results, checkov_results)))
+    
+
+def deduplicate_results(trivy_results: dict, checkov_results: dict) -> dict:
+    deduped_issues = dict() # dictionary key (which must be unique) is a tuple representing the resource and the normalized key
+
+    # checkov first, since it doesn't have a resolution field and is often missing severity
+    # we want to overwrite duplicate findings with the Trivy entry that DOES have resolution details
+    for finding in checkov_results["results"]["failed_checks"]:
+        key = normalize_key(finding["resource"], finding["check_name"])
+        deduped_issues[key] = (map_to_owasp(key), "checkov", finding["check_id"], None, finding["severity"], finding["file_line_range"][0], finding["file_line_range"][1])
+
+    for target in trivy_results["Results"]:
+        if target["MisconfSummary"]["Failures"] > 0:
+            for finding in target["Misconfigurations"]:
+                key = normalize_key(finding["CauseMetadata"]["Resource"], finding["Title"])
+                deduped_issues[key] = (map_to_owasp(key),"trivy", finding["ID"],finding["Resolution"], finding["Severity"], finding["CauseMetadata"]["StartLine"], finding["CauseMetadata"]["EndLine"])
+
+    return deduped_issues
+
+def normalize_key(resource: str, title: str) -> str:
+    title_lower = title.lower()
+    rtype = resource.split(".")[0]
+    for (rprefix, keyword), deduped in DEDUP_MAP.items():
+        if rprefix == rtype and keyword in title_lower:
+            return f"{resource}::{deduped}"
+    return f"{resource}::{title_lower}"  # fallback: no dedup
+
+def map_to_owasp(issue_name: str) -> str:
+    for keyword, owasp in OWASP_MAP.items():
+        if keyword in issue_name:
+            return owasp
+    return "A02:2025 - Security Misconfiguration" # default category
+
+if __name__ == "__main__":
+    parse_results("trivy-results.json", "checkov-results/results_json.json")
