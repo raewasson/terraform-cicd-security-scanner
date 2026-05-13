e2d8d5d5f163ad95eaf2799f1a453f83087d08a9
raewasson <rachaelwasson2000@gmail.com>
2026-05-13 09:56:26 -0500

    feat: fail on high+ findings

--- scanner/scan.py
+++ scanner/scan.py
@@ -4,6 +4,7 @@ Trivy and Checkov create a lot of duplicates, this script is a quick poc for ded
 
 import json
 import pandas as pd
+from collections import Counter
 
 DEDUP_MAP = {
     # (resource_type_prefix, keyword) -> deduped issue name
@@ -18,22 +19,30 @@ OWASP_MAP = {
     "logging": "OWASP A09:2025 - Security Logging and Alerting Failures"
 }
 
-def parse_results(trivy_path: str, checkov_path: str):
+def parse_results(trivy_path: str, checkov_path: str) -> bool:
     with open(trivy_path) as trivy_file:
       trivy_results = json.load(trivy_file)
     with open(checkov_path) as checkov_file:
       checkov_results = json.load(checkov_file)
     results = deduplicate_results(trivy_results, checkov_results)
-    with open("security_report.md", "w") as f: f.write(results_to_markdown(results))
+    # only trivy findings have severity
+    severity_counts = Counter(issue[0].upper() for issue in results.values() if issue[2] == "trivy")
+    with open("security_report.md", "w") as f: f.write(
+        f"Total: {len(results)} (CRITICAL: {severity_counts['CRITICAL']}, HIGH: {severity_counts['HIGH']})" +
+        results_to_markdown(results)
+    )
+    if severity_counts['CRITICAL'] + severity_counts['HIGH'] > 0:
+        return True
+    return False
 
 def deduplicate_results(trivy_results: dict, checkov_results: dict) -> dict:
     deduped_issues = dict() # use uniqueness of dict keys to dedupe, after normalization of keys Trivy may overwrite Checkov findings
 
-    # checkov first, since it doesn't have a resolution field and is often missing severity
+    # checkov first, since it doesn't have a resolution field or severity (because not using a prisma cloud api key)
     # we want to overwrite duplicate findings with the Trivy entry that DOES have resolution details
     for finding in checkov_results["results"]["failed_checks"]:
         key = normalize_key(finding["resource"], finding["check_name"])
-        deduped_issues[key] = (finding ["severity"], map_to_owasp(key), "checkov", finding["check_id"], None, finding["file_line_range"][0], finding["file_line_range"][1])
+        deduped_issues[key] = (finding["severity"], map_to_owasp(key), "checkov", finding["check_id"], None, finding["file_line_range"][0], finding["file_line_range"][1])
 
     for target in trivy_results["Results"]:
         if target["MisconfSummary"]["Failures"] > 0:
@@ -64,4 +73,4 @@ def results_to_markdown(deduped_results: dict) -> str:
     return markdown_table
 
 if __name__ == "__main__":
-    parse_results("trivy-results.json", "checkov-results/results_json.json")
+    print(parse_results("trivy-results.json", "checkov-results/results_json.json"))
